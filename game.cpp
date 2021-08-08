2e1f72425550e7dc46e80f27a286c31688c603ea
D0nni387 <Richard.wells1987@hotmail.co.uk>
2021-08-08 16:48:13 +0100

    added basic sums and additional user information

--- game.cpp
+++ game.cpp
@@ -2,7 +2,22 @@
 
 int main()
 {
+    // On game load, displayed welcome message to user
     std::cout << "Waking up to the sound of your jail cell door unlocking, you leave to find the placed deserted \n";
-    std::cout << "The only way you can escape is to get through some strange doors locked with number codes and puzzles";
+    std::cout << "The only way you can escape is to get through some strange doors locked with number codes and puzzles \n";
+    std::cout << "Splashed on the walls in what might be blood is the message, 'Three numbers is all i needed' \n";
+    std::cout << "There is no body around.... i wonder who did this?! \n";
+
+    // Variables
+    int a = 4;
+    int b = 3;
+    int c = 1;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << product;
+
     return 0;
 }
