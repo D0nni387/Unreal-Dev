6d25e30abaab381d9dd159814cd2ed32f174ec04
D0nni387 <Richard.wells1987@hotmail.co.uk>
2021-08-08 17:42:24 +0100

    added user input field and var print to terminal

--- game.cpp
+++ game.cpp
@@ -9,18 +9,24 @@ int main()
     std::cout << "There is no body around.... i wonder who did this?! \n \n";
 
     // Variables
-    int a = 4;
-    int b = 3;
-    int c = 1;
+    int CodeA = 4;
+    int CodeB = 3;
+    int CodeC = 1;
 
-    int sum = a + b + c;
-    int product = a * b * c;
+    int CodeSum = CodeA + CodeB + CodeC;
+    int CodeProduct = CodeA * CodeB * CodeC;
 
 
     std::cout << "+ There are 3 numbers written on the floor this must be a clue to the code" << std::endl << "+ Adds to: ";
-    std::cout << sum << std::endl;
+    std::cout << CodeSum << std::endl;
     std::cout << "+ Multiply to: ";
-    std::cout << product;
+    std::cout << CodeProduct << std::endl;
+
+    int PlayerGuess;
+    std::cout << "Enter the code: ";
+    std::cin >> PlayerGuess;
+
+    std::cout << PlayerGuess;
 
     return 0;
 }
