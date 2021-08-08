283ba4bf3cc051396097380e0a3e06b6348c481a
D0nni387 <Richard.wells1987@hotmail.co.uk>
2021-08-08 17:50:03 +0100

    Added guess sum and product calcs

--- game.cpp
+++ game.cpp
@@ -6,7 +6,7 @@ int main()
     std::cout << "Waking up to the sound of your jail cell door unlocking, you leave to find the placed deserted \n";
     std::cout << "The only way you can escape is to get through some strange doors locked with number codes and puzzles \n";
     std::cout << "Splashed on the walls in what might be blood is the message, 'Three numbers is all i needed' \n";
-    std::cout << "There is no body around.... i wonder who did this?! \n \n";
+    std::cout << "There is nobody around.... i wonder who did this?! \n \n";
 
     // Variables
     int CodeA = 4;
@@ -22,11 +22,14 @@ int main()
     std::cout << "+ Multiply to: ";
     std::cout << CodeProduct << std::endl;
 
-    int PlayerGuess;
-    std::cout << "Enter the code: ";
-    std::cin >> PlayerGuess;
+    int GuessA, GuessB, GuessC;
+    std::cout << "+ Enter the 3 numbers seperated by a space to unlock the door: ";
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
 
-    std::cout << PlayerGuess;
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
 
     return 0;
 }
