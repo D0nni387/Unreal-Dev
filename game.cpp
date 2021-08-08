e35b0c2eb639d6dea8181200831ac6f8f9ab2346
D0nni387 <Richard.wells1987@hotmail.co.uk>
2021-08-08 16:06:51 +0100

    added game intro text

--- game.cpp
+++ game.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    std::cout << "Hello there!";
+    std::cout << "Waking up to the sound of your jail cell door unlocking, you leave to find the placed deserted \n";
+    std::cout << "The only way you can escape is to get through some strange doors locked with number codes and puzzles";
     return 0;
 }
